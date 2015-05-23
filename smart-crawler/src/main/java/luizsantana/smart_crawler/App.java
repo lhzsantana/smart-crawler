59d5a711b83dc7133af957a1c8b18a9cf6900be5
Luiz Santana <lhzsantana@hotmail.com>
2015-05-23 14:50:43 -0300

    Initial commit

---
+++ smart-crawler/src/main/java/luizsantana/smart_crawler/App.java
@@ -0,0 +1,13 @@
+package luizsantana.smart_crawler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
